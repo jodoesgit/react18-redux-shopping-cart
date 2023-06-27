f5e98392f63f324b45711b221d93f26c13d529f0
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-26 18:35:45 -0700

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { CartIcon } from "../icons";
+import { UseSelector } from "react-redux";
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div className="nav-center">
+				<h3>Redux Toolkit</h3>
+				<div className="nav-container">
+					<CartIcon />
+					<div className="amount-container">
+						<p className="total-amount">0</p>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+export default Navbar;
