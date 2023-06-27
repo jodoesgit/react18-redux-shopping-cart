46c0a2283f4993ea43e35b55da845d5927a0cae8
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-26 18:51:45 -0700

    Setup CartContainer/CartItem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import { CartIcon } from "../icons";
-import { UseSelector } from "react-redux";
+import { UseSelector, useSelector } from "react-redux";
 
 const Navbar = () => {
+	const { amount } = useSelector((store) => store.cart);
+
 	return (
 		<nav>
 			<div className="nav-center">
-				<h3>Redux Toolkit</h3>
+				<h3>Shopping Cart</h3>
 				<div className="nav-container">
 					<CartIcon />
 					<div className="amount-container">
-						<p className="total-amount">0</p>
+						<p className="total-amount">{amount}</p>
 					</div>
 				</div>
 			</div>
